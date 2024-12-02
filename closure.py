384aaa9ac6ca90038e6c0bcaf88eb03da63064ba
BruxBel <aleksei.geek.rus@gmail.com>
2024-12-03 02:07:16 +0300

    add new_attr

--- closure.py
+++ closure.py
@@ -2,6 +2,12 @@ class RootCalculator:
     def __init__(self, root_degree, precision=2):
         self.root_degree = root_degree
         self.precision = precision
+        # Изменение: добавляем новый атрибут в одной ветке
+        self.method_type = "basic"  # Это добавление для создания конфликта
 
     def __call__(self, number):
         return round(pow(number, 1 / self.root_degree), self.precision)
+
+    # Изменение: добавление метода в одной ветке
+    def calculate_root(self, number):
+        return pow(number, 1 / self.root_degree)
